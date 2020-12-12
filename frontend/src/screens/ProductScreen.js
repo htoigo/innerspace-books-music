8c2d7f84d70ae9f27acaaf4dd24227f31f67c590
Harry H. Toigo <hhtoigo@gmail.com>
2020-12-11 23:27:18 -0800

    Modify Home and Product screens to get data from Redux store using actions.
    Also added conditional rendering to Home screen so that it shows a loading spinner while
    waiting for the list of products to be fetched from the backend. Added code to
    display a MessageBox when there is an error.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,60 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data';
 
 export default function ProductScreen(props) {
-  const product = data.products.find(x => x._id === props.match.params.id);
-  if (!product) {
-    return <div>Product not found.</div>;
-  }
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector(state => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
   return (
-      <div>
-        <Link to="/"> Back to results. </Link>
-        <div className="row top">
-          <div className="col-double">
-            <img className="large" src={product.image} alt={product.name} />
-          </div>
-          <div className="col-single">
-            <ul>
-              <li><h1>{product.name}</h1></li>
-              <li>
-                <Rating rating={product.rating} numReviews={product.numReviews} />
-              </li>
-              <li> Price: ${product.price} </li>
-              <li> Description: <p>{product.description}</p> </li>
-            </ul>
-          </div>
-          <div className="col-single">
-            <div className="card card-body">
-              <ul>
-                <li>
-                  <div className="row">
-                    <div> Price </div>
-                    <div className="price"> ${product.price} </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="row">
-                    <div> Status </div>
-                    <div>
-                      {
-                        product.countInStock > 0
-                          ? <span className="success"> In Stock </span>
-                          : <span className="warning"> Unavailable </span>
-                      }
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <button className="primary block">
-                    Add to Cart
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      {loading
+         ? <LoadingBox />
+         : error
+             ? <MessageBox variant="warning"> {error} </MessageBox>
+             : (
+               <div>
+                 <Link to="/"> Back to results. </Link>
+                 <div className="row top">
+                   <div className="col-double">
+                     <img className="large" src={product.image} alt={product.name} />
+                   </div>
+                   <div className="col-single">
+                     <ul>
+                       <li><h1>{product.name}</h1></li>
+                       <li>
+                         <Rating rating={product.rating} numReviews={product.numReviews} />
+                       </li>
+                       <li> Price: ${product.price} </li>
+                       <li> Description: <p>{product.description}</p> </li>
+                     </ul>
+                   </div>
+                   <div className="col-single">
+                     <div className="card card-body">
+                       <ul>
+                         <li>
+                           <div className="row">
+                             <div> Price </div>
+                             <div className="price"> ${product.price} </div>
+                           </div>
+                         </li>
+                         <li>
+                           <div className="row">
+                             <div> Status </div>
+                             <div>
+                               {
+                                 product.countInStock > 0
+                                   ? <span className="success"> In Stock </span>
+                                   : <span className="warning"> Unavailable </span>
+                               }
+                             </div>
+                           </div>
+                         </li>
+                         <li>
+                           <button className="primary block">
+                             Add to Cart
+                           </button>
+                         </li>
+                       </ul>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             )
+      }
+    </div>
   );
 }
