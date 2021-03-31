02b7f91581d097834a0a77e03cf60cf285867574
Harry H. Toigo II <hhtoigo@gmail.com>
2021-03-31 16:42:34 -0700

    Change <a> elements to <Link> in Product component, befitting a Single page app.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 export default function Product(props) {
@@ -6,14 +7,14 @@ export default function Product(props) {
 
   return (
     <div key={product._id} className="card">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <img className="medium" src={product.image}
              alt={product.name} />
-      </a>
+      </Link>
       <div className="card-body">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <h2> {product.title} </h2>
-        </a>
+        </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <div className="price"> ${product.price} </div>
       </div>
