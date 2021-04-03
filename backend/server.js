6ff7982fbb8e7002d27fb372d83d2183b9bbf959
Harry H. Toigo II <hhtoigo@gmail.com>
2021-04-02 21:11:59 -0700

    Move product-related routes from server.js to productRouter.js in backend.
    In backend/server.js, we remove the route handlers for '/api/products' and
    '/api/products/:id', and use app.use() to refer all routes matching
    '/api/products' to productRouter, defined in routers/productRouter.js.
    
    The route /api/products is now handled as “/” in productRouter.js and
    the /api/products/:id is now the handled as “/:id” in productRouter.js. We also
    defined a new handler for the route “/seed”, which is used to seed the MongoDB
    database with sample products from the array in data.js.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
 
 const app = express();
 
@@ -15,19 +15,7 @@ mongoose.connect(
 );
 
 app.use('/api/users', userRouter);
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find(x => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({message: 'Product not found.'});
-  }
-});
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+app.use('/api/products', productRouter);
 
 app.get('/', (req, res) => {
   res.send('Express Server at your service.');
