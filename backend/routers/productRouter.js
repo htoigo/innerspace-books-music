5c24a9ee95c7c69587fda70580d6cf9920370eda
Harry H. Toigo II <hhtoigo@gmail.com>
2021-11-10 16:23:23 -0800

    backend/routers/productRouter.js re-formatted by Prettier.

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -6,7 +6,7 @@ import data from '../data.js';
 const productRouter = express.Router();
 
 productRouter.get(
-  "/",
+  '/',
   expressAsyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.send(products);
@@ -14,27 +14,26 @@ productRouter.get(
 );
 
 productRouter.get(
-  "/seed",
+  '/seed',
   expressAsyncHandler(async (req, res) => {
     // await Product.deleteMany({});
 
     const createdProducts = await Product.insertMany(data.products);
-    res.send( {createdProducts} );
+    res.send({ createdProducts });
   })
 );
 
 // Must be after handler for '/seed', bc '/:id' would match '/seed'.
 productRouter.get(
-  "/:id",
+  '/:id',
   expressAsyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (product) {
       res.send(product);
     } else {
-      res.status(404).send({message: 'Product not found.'});
+      res.status(404).send({ message: 'Product not found.' });
     }
   })
 );
-
 
 export default productRouter;
