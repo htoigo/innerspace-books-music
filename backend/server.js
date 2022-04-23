3f62176580a49e6557df7036f212e4d056aca9a1
Harry H. Toigo II <hhtoigo@gmail.com>
2022-04-22 18:07:37 -0700

    Made modifications for publishing the app to Heroku.
    In backend/server.js, we added app.use(express.static()) to allow
    Heroku to serve the React frontend files from the build folder in the
    frontend. Also, added app.get('*', ) so GET requests to all URLs
    return the index.html file in the frontend, and we removed the old
    app.get('/'...) which returned the message, "Express Server at your
    service."
    
    In the package.json file, we added a new key "engines" with value
    "node": "17.9.0" and "npm": "8.5.5", which specifies to Heroku the
    versions of Node.js and npm that this app should use.
    
    We also added a build script to the package.json file that will be
    used by Heroku.

--- backend/server.js
+++ backend/server.js
@@ -31,11 +31,17 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// The following allows Heroku to serve the React files in the build folder in
+// frontend.
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Express Server at your service.');
+// });
 
-app.get('/', (req, res) => {
-  res.send('Express Server at your service.');
-});
-
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
